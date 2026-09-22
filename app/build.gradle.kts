378906cf2c2345bdc2f24b7366b70f03cb110921
dziambor-glitch <dziambor@gmail.com>
2026-09-22 23:36:53 +0200

    Octopus UX: bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.pvcompact.app.clean"
         minSdk = 26
         targetSdk = 36
-        versionCode = 122
-        versionName = "1.2.2"
+        versionCode = 130
+        versionName = "1.3.0"
     }
 
     buildTypes {
